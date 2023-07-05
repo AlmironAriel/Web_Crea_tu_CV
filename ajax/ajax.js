598f2e0ea6fa5ef8d6920b86904846b133dbd71d
Leandrocode1 <leandrocabj99@gmail.com>
2023-07-05 00:14:42 -0300

    Validacion de datos

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+   var reg = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/; 
    var datos = [];
 
     $('#name').mouseover(function(){
@@ -8,13 +9,15 @@ $(document).ready(function () {
     });
     $('#name').mouseout(function () {
        var nombre=$('#name').val();
-       if(nombre !== ''){
-           $('#name').css({
+        if (nombre && nombre.trim().length > 2 && /^[A-Z].*/g.test(nombre) && nombre[0] === nombre[0].toUpperCase()){
+            $('#mensaje_nombre_error').fadeOut(); 
+            $('#name').css({
                'border': '3px solid green'
            });
             datos.push('nombre:',nombre);
             console.log(datos);
        }else{
+           $('#mensaje_nombre_error').fadeIn(); 
            $('#name').css({
                'border': '3px solid red'
            });
@@ -28,17 +31,62 @@ $(document).ready(function () {
     });
     $('#apellido').mouseout(function () {
         var apellido = $('#apellido').val();
-        if (apellido !== '') {
+        if (apellido && apellido.trim().length > 2 && /^[A-Z].*/g.test(apellido) && apellido[0] === apellido[0].toUpperCase()) {
+            $('#mensaje_apellido_error').fadeOut(); 
             $('#apellido').css({
                 'border': '3px solid green'
             });
             datos.push('apellido:',apellido);
             console.log(datos);
         } else {
+            $('#mensaje_apellido_error').fadeIn(); 
             $('#apellido').css({
                 'border': '3px solid red'
             });
         }
+    });
+
+    $('#dni').mouseover(function () {
+        $('#dni').css({
+            'border': '3px solid blue'
+        });
+    });
+    $('#dni').mouseout(function () {
+        var dni = $('#dni').val();
+        if (dni && dni.trim().length > 2 && /^[0-9].*/g.test(dni)) {
+            $('#mensaje_dni_error').fadeOut();
+            $('#dni').css({
+                'border': '3px solid green'
+            });
+            datos.push('dni:', dni);
+            console.log(datos);
+        } else {
+            $('#mensaje_dni_error').fadeIn();
+            $('#dni').css({
+                'border': '3px solid red'
+            });
+        }
+    });
+
+
+    $('#email').mouseover(function(){
+        $('#email').css({
+            'border': '3px solid blue'
+        }); 
+    });
+    $('#email').mouseout(function () {
+        var email = $('#email').val();
+        if(email =="" || !reg.test(email)){
+            $('#mensaje_email_error').fadeIn();
+            $('#email').css({
+                'border': '3px solid red'
+            });
+        }else{
+            $('#email').css({
+                'border': '3px solid green'
+            });
+        }
+        
     });
 
     $('#dataForm').submit(function (e) {
@@ -59,6 +107,10 @@ $(document).ready(function () {
                 console.log(jsonData);
 
                 $('#nombreModal').html(jsonData.nombre);
+                $('#apellidoModal').html(jsonData.apellido);
+                $('#dniModal').html(jsonData.dni);
+                $('#estado_civilModal').html(jsonData.estadoCivil);
+                $('#emailModal').html(jsonData.email);
             }
         })
     });
--- php/data.php
+++ php/data.php
@@ -1,12 +1,13 @@
 <?php
 $nombre= $_POST['name'];
 $apellido = $_POST['apellido'];
+$dni= $_POST['dni'];
+$estado_civil = $_POST['estado_civil'];
+$email = $_POST['email'];
 
 
 
-
-
-$data = array("nombre" => $nombre, 'apellido' => $apellido);
+$data = array("nombre" => $nombre, 'apellido' => $apellido, "dni"=>$dni, "estadoCivil"=>$estado_civil,"email"=>$email);
 
 echo json_encode($data, JSON_PRETTY_PRINT);
 ?>
